0e0099275b025ea81b5bbb046686d79ff5d73fb5
Sonja Wild <swild@kn-n010.top.orn.mpg.de>
2022-01-04 12:35:15 +0100

    # added latency

--- fledgie_learning_NBDA.R
+++ fledgie_learning_NBDA.R
@@ -1,43 +1,43 @@
 
 # 1) Install and load libraries -------------------------------------------
 
-# NBDA
-
+# load NBDa via devtools
+library(devtools)
+# install_github("whoppitt/NBDA")
+library(NBDA)
 
 
 # 2) Data preparation -----------------------------------------------------
 
 
-# 2.1) Load network data --------------------------------------------------
+# 2.1) Load network data and puzzle data --------------------------------------------------
 
 setwd("C:/Users/swild/Desktop/Konstanz/Breeding Season 2021/Fledgie social learning/Data")
 
 load("Guett.gmm.RDA")
 load("Mill.gmm.RDA")
 
+load("Mill.puzzle.data.RDA")
+load("Guett.puzzle.data.RDA")
+
+# load puzzle data from previous winter
+load("puzzle.data.winter20.RDA")
+
+
 
 # 2.2. Load breeder data --------------------------------------------------
 
 fledge.data <- read.delim("fledge.dates.txt", sep=" ")
 breeders <- read.delim("Breeders 2021.txt")
+PITs <- read.delim("Species_PITs.txt")
 
 # Make a list of great tit chicks
 GT.chicks <- subset(breeders$Tag, breeders$Who=="Chick" & breeders$Species=="GRETI" & breeders$Tag!="")
 GT.chicks <- GT.chicks[!is.na(GT.chicks)]
 
+GT.list <- subset(PITs$Pit, PITs$Species=="GRETI")
 
 # 2.3. Extract independence day for fledgies ------------------------------
-
-# read in GBI data
-
-GBI_Mill_1_13 <- read.delim("week1-13.gbi.Mill.txt", sep=",", check.names = FALSE)
-head(GBI_Mill_1_13)
-
-
-# read in chick list
-chicks.df <- read.delim("greti.chicks.2020.txt", sep="\t")
-chicks <- as.character(na.omit(subset(chicks.df$Tag, subset=chicks.df$Who=="Chick")))
-
 ######
 # extract the day where chick independent from parents
 # i.e. the first day where the number of visits with parents is smaller or equal than the number of visits with other adults (when parents were not present)
@@ -45,7 +45,6 @@ chicks <- as.character(na.omit(subset(chicks.df$Tag, subset=chicks.df$Who=="Chic
 
 `%notin%` <- Negate(`%in%`)
 
-data <- Guett.gmm
 
 independent.chicks <- function(data, site){ 
   
@@ -113,4 +112,187 @@ independent.chicks <- function(data, site){
 independence.Guett <- independent.chicks(data=Guett.gmm, site="Guett")
 independence.Mill <- independent.chicks(data=Mill.gmm, site="Castle")
 
+
+# 2.4. Make a list of IDs to include --------------------------------------
+
+# we include all birds who have been seen at the network feeders at least 10 times
+# regardless of whether they solved the puzzle box or not
+
+# Mill
+
+Mill.IDs <- colnames(Mill.gmm$gbi[,colSums(Mill.gmm$gbi)>=10])
+length(Mill.IDs)
+# 234 tagged birds were recorded at the network feeders at least 10 times
+# reduce to great tits only
+Mill.IDs <- subset(Mill.IDs, Mill.IDs %in% GT.list)
+length(Mill.IDs)
+# 170 great tits with at least 10 sightings
+
+# Guett
+Guett.IDs <- colnames(Guett.gmm$gbi[,colSums(Guett.gmm$gbi)>=10])
+length(Guett.IDs)
+# 79 tagged birds were recorded at the network feeders at least 10 times
+
+# reduce to great tits only
+Guett.IDs <- subset(Guett.IDs, Guett.IDs %in% GT.list)
+length(Guett.IDs)
+# 61 great tits with at least 10 sightings
+
+# 2.5. Create vertical social network -------------------------------------
+
+create.vert.net <- function(IDs){
+  mat <- matrix(0, nrow=length(IDs), ncol=length(IDs))
+  rownames(mat) <- IDs
+  colnames(mat) <- IDs
+  
+  # chick IDs
+  IDs.sub <- IDs[IDs %in% breeders$Tag & IDs %in% GT.chicks]
+  
+  for(i in IDs.sub){
+    # extract the info for the bird i
+    i.box <- subset(breeders$Box, breeders$Tag==i)
+    # extract the parents of that chick
+    parents <- subset(breeders$Tag, breeders$Box==i.box & breeders$Who %in% c("Male", "Female"))
+    parents <- subset(parents, parents %in% IDs)
+    # set entries between parents and offspring to 1 in the direction of the chick learning from parents
+    # but not the other way round
+    # SW: double check direction is correct
+    mat[i, parents] <- 1
+    
+  }  
+  
+ return(mat)   
+}
+
+Mill.vert.matrix <- create.vert.net(Mill.IDs)
+Guett.vert.matrix <- create.vert.net(Guett.IDs)
+
+
+# 2.6. Create oblique social network --------------------------------------
+
+create.obl.net <- function(IDs){
+  mat <- matrix(0, nrow=length(IDs), ncol=length(IDs))
+  rownames(mat) <- IDs
+  colnames(mat) <- IDs
+  
+  # chick IDs
+  IDs.sub <- IDs[IDs %in% breeders$Tag & IDs %in% GT.chicks]
+  
+  for(i in IDs.sub){
+    # extract the info for the bird i
+    i.box <- subset(breeders$Box, breeders$Tag==i)
+    # extract the adult non-parents of that chick
+    adults <- subset(breeders$Tag, breeders$Who %in% c("Male", "Females") & breeders$Box != i.box)
+    adults <- subset(adults, adults %in% IDs)
+    # set entries between non-parent adults and offspring to 1 in the direction of the chick learning from adults
+    # but not the other way round
+    # SW: double check direction is correct
+    mat[i, adults] <- 1
+    
+  }  
+  
+  return(mat)   
+}
+
+Mill.obl.matrix <- create.obl.net(IDs = Mill.IDs)
+Guett.obl.matrix <- create.obl.net(IDs = Guett.IDs)
+
+
+
+# 2.7. Create horizontal social network -----------------------------------
+
+create.hor.net <- function(IDs){
+  mat <- matrix(0, nrow=length(IDs), ncol=length(IDs))
+  rownames(mat) <- IDs
+  colnames(mat) <- IDs
+  
+  # chick IDs
+  IDs.sub <- IDs[IDs %in% breeders$Tag & IDs %in% GT.chicks]
+  
+  # set connections among all chicks to 1
+  # they can learn in both directions
+  mat[IDs.sub, IDs.sub] <- 1
+  
+  return(mat)   
+}
+
+Mill.hor.matrix <- create.hor.net(IDs = Mill.IDs)
+Guett.hor.matrix <- create.hor.net(IDs = Guett.IDs)
+
+
+# 2.8. Extract the demonstrators ----------------------------------------
+
+# first we extract all the knowledgeable birds until the day that the first fledgling left the nest
+min(fledge.data$Date.Time[fledge.data$Date.Time!=""])
+# "2021-05-14 02:00:00" # the first fledgie fledged on the 14.05.2021
+# everybody who knows how to solve up to this point (with at least 3 solves) is considered a demonstrator
+
+# first we need to consider those who have learned during the winter experiment (20/21), but have not produced any solutions in spring up until this day
+# we first subset the data to solves
+puzzle.data.winter20.combined <- rbind(puzzle.data.winter20$Guett, puzzle.data.winter20$Mill)
+puzzle.data.winter20.combined <- subset(puzzle.data.winter20.combined, puzzle.data.winter20.combined$event %in% c("right", "left"))
+num.solves.winter20 <- as.data.frame(table(puzzle.data.winter20.combined$PIT))
+solvers.winter20 <- as.vector(subset(num.solves.winter20$Var1, num.solves.winter20$Freq>=3))
+
+# now we extract the knowledgeable birds from the breeding season 2021 up until the 14th of May 2021
+puzzle.data.spring21.combined <- rbind(Guett.puzzle.data, Mill.puzzle.data)
+puzzle.data.spring21.combined <- subset(puzzle.data.spring21.combined, puzzle.data.spring21.combined$Event %in% c("right", "left") & puzzle.data.spring21.combined$Date.Time < "210514000000")
+num.solves.spring21 <- as.data.frame(table(puzzle.data.spring21.combined$PIT))
+solvers.spring21 <- as.vector(subset(num.solves.spring21$Var1, num.solves.spring21$Freq>=3))
+
+# combining the two
+demos <- unique(c(solvers.winter20, solvers.spring21))
+# these are all knowledgeable birds from Mill and Guett up until the 14th of May 2021
+
+
+# 2.9. Extract date of acquisition for fledgies ---------------------------
+
+extract.acquisition.date <- function(puzzle.data){
+  sites <- sort(unique(puzzle.data$Location))
+  # extract those with at least three solves
+  
+  puzzle.data.sub <- subset(puzzle.data, puzzle.data$Event %in% c("right", "left"))
+  solvers <- as.data.frame(table(puzzle.data.sub$PIT))
+  solvers <- as.vector(subset(solvers$Var1, solvers$Freq>=3))
+  # remove demonstrators
+  solvers <- subset(solvers, !(solvers %in% demos ))
+  
+  # no extract the date of the first solves at each of the puzzle sites (if applicable)
+  # we consider the bird a learner at the site that it first produced the solution
+  # in all other sites, it will be filtered from the diffusion (it can still transmit behaviour but it is not considered in the model)
+  data.all <- NULL
+  for(i in solvers){
+  acq.dates <- NULL
+      for(j in sites){
+      sub.i.j <- subset(puzzle.data.sub, puzzle.data.sub$PIT==i & puzzle.data.sub$Location==j)
+      if(length(sub.i.j$PIT)==0){
+        date.first.solve.j <- NA
+      } else {
+        date.first.solve.j <- min(sub.i.j$Date.Time)  
+      }
+      acq.dates[which(sites==j)] <- date.first.solve.j
+      }
+  i.data <- c(i, acq.dates)
+  data.all <- rbind(data.all, i.data)
+  }
+  data.all <- as.data.frame(data.all)
+  rownames(data.all) <- NULL
+  colnames(data.all) <- c("PIT", sites)
+  return(data.all)
+}
+
+
+Mill.acquisition.date <- extract.acquisition.date(puzzle.data = Mill.puzzle.data)
+
+Guett.acquisition.date <- extract.acquisition.date(puzzle.data = Guett.puzzle.data)
+
+
+# 3) NBDA ------------------------------------------------------------------
+
+
+# 3.1. Prepare NBDA data objects ------------------------------------------
+
+prepare.NBDA.object <- function(networks, acquisition.dates, demos){
+  
+}
 
